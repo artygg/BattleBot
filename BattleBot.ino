28e5ef7e6a54de0a951898803b74efcd6652cb3a
artygg <artjoms.grisajevs@student.nhlstenden.com>
2024-02-08 11:29:12 +0100

    Edit: motors

--- BattleBot.ino
+++ BattleBot.ino
@@ -1,27 +1,125 @@
+#include <Servo.h>
+
 // Define pins for motor 1
-int pwmPinMotor1 = 3; // PWM pin for motor 1
-int dirPinMotor1 = 4; // Direction pin for motor 1
+const int a1Motor1 = 11; // PWM pin for motor 1
+const int a2Motor1 = 10; // Direction pin for motor 1
+const int sensorPinMotor1 = 8; //Sensor pin for motor 1
 
 // Define pins for motor 2
-int pwmPinMotor2 = 6; // PWM pin for motor 2
-int dirPinMotor2 = 5; // Direction pin for motor 2
+const int a1Motor2 = 5; // PWM pin for motor 2
+const int a2Motor2 = 6; // Direction pin for motor 2
+const int sensorPinMotor2 = 9; //Sensor pin for motor 2
+
+
+const int trigPin = 2;  
+const int echoPin = 7; 
+
+
+Servo myservo;
+
+const int speed1 = 235;
+const int speed2 = 250;
+
+float duration, distance;  
+int sensorValueMotor1 = 0;
+int sensorValueMotor2 = 0;
 
 void setup() {
     // Initialize PWM and direction pins as outputs
-    pinMode(pwmPinMotor1, OUTPUT);
-    pinMode(dirPinMotor1, OUTPUT);
-    pinMode(pwmPinMotor2, OUTPUT);
-    pinMode(dirPinMotor2, OUTPUT);
+    pinMode(trigPin, OUTPUT);  
+  	pinMode(echoPin, INPUT);  
+	  Serial.begin(9600);  
+
+    pinMode(a1Motor1, OUTPUT);
+    pinMode(a2Motor1, OUTPUT);
+    pinMode(a1Motor2, OUTPUT);
+    pinMode(a2Motor2, OUTPUT);
+    myservo.attach(3);
+    //pinMode(sensorPinMotor1, INPUT_PULLUP);
+    //pinMode(sensorPinMotor2, INPUT_PULLUP);
+    open();
+    delay(4000);
+    close();
+
 }
 
 void loop() {
-    // Set direction and PWM duty cycle for motor 1 (e.g., half-speed forward)
-    digitalWrite(dirPinMotor1, HIGH); // Set direction forward
-    analogWrite(pwmPinMotor1, 250); // Example: 50% duty cycle for motor 1
+  forward();
+    sensorValueMotor1 = digitalRead(sensorPinMotor1);
+    sensorValueMotor2 = digitalRead(sensorPinMotor2);
+    int speedAdjustment = 0;
+    if (sensorValueMotor1 == HIGH && sensorValueMotor2 == LOW) {
+        speedAdjustment = 5; // Increase speed of motor 2
+    } else if (sensorValueMotor1 == LOW && sensorValueMotor2 == HIGH) {
+        speedAdjustment = -5; // Decrease speed of motor 2
+    }*/
+    //forward(a1Motor1, a2Motor1, 235);
+    //forward(a1Motor2, a2Motor2, 250);
+    //Serial.println(speedAdjustment);
 
-    // Set direction and PWM duty cycle for motor 2 (e.g., quarter-speed backward)
-    digitalWrite(dirPinMotor2, HIGH); // Set direction backward
-    analogWrite(pwmPinMotor2, 250); // Example: 25% duty cycle for motor 2
+    /*digitalWrite(trigPin, LOW);  
+	  delayMicroseconds(2);  
+	  digitalWrite(trigPin, HIGH);  
+	  delayMicroseconds(10);  
+	  digitalWrite(trigPin, LOW);  
 
-    delay(500); // Delay for 1 second Hello
+    duration = pulseIn(echoPin, HIGH);  
+    distance = (duration*.0343)/2;  
+    Serial.print("Distance: ");  
+    Serial.println(distance);  
+    delay(100); 
+    if(distance < 30) {
+      back();
+      delay(3000);
+      right();
+      delay(500);
+    }*/
+
+  
+  forward();
+  delay(3000);
+  back();
+  delay(3000);
+  left();
+  delay(3000);
+  right();
+  delay(3000);
+
+}
+
+void stop(){
+  drive(a1Motor1, a2Motor1, 0);
+  drive(a1Motor2, a2Motor2, 0);
+}
+
+void left() {
+  drive(a1Motor1, a2Motor1, 0);
+  drive(a1Motor2, a2Motor2, 250);
+}
+
+void right() {
+  drive(a1Motor1, a2Motor1, 235);
+  drive(a1Motor2, a2Motor2, 0);
+}
+
+void forward() {
+  drive(a1Motor1, a2Motor1, 235);
+  drive(a1Motor2, a2Motor2, 250);
+}
+void back() {
+  drive(a2Motor1, a1Motor1, 235);
+  drive(a2Motor2, a1Motor2, 250);
+}
+
+void drive(int a1, int a2, int speed) {
+  digitalWrite(a1, speed);
+  digitalWrite(a2, LOW);
+}
+
+void open(){
+  myservo.write(120);
+}
+
+void close(){
+  myservo.write(0);
 }
